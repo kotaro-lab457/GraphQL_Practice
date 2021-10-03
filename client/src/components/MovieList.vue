b3ee07221a32a3f17e3cd1796a1da83532b24e90
kotaro-lab457 <k.chanmpmp@icloud.com>
2021-10-03 18:59:17 +0900

    add: UI実装 Header, MovieList, Sidena

---
+++ client/src/components/MovieList.vue
@@ -0,0 +1,28 @@
+<template>
+  <b-table-simple outlined hover class="mt-4">
+    <b-thead>
+      <b-tr>
+        <b-th>タイトル</b-th>
+        <b-th>ジャンル</b-th>
+        <b-th>監督</b-th>
+      </b-tr>
+    </b-thead>
+    <b-tbody>
+      <b-tr>
+        <b-td>となりのトトロ</b-td>
+        <b-td>アニメ</b-td>
+        <b-td>宮崎駿</b-td>
+      </b-tr>
+    </b-tbody>
+  </b-table-simple>
+</template>
+
+<script>
+  export default {
+    name: 'MovieList'
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
