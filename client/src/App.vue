699ab28c48ad0472fd825dd5a0ef2d2e67384c51
kotaro-lab457 <k.chanmpmp@icloud.com>
2021-10-03 19:00:12 +0900

    add: GraphQL client side 設定

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,17 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+    <b-container>
+  <b-row>
+    <b-col md='4'>
+      <Sidenav />
+    </b-col>
+    <b-col md='8'>
+      <MovieList />
+    </b-col>
+  </b-row>
+</b-container>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header';
+import MovieList from './components/MovieList';
+import Sidenav from './components/Sidenav';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    MovieList,
+    Sidenav,
   }
 }
 </script>
--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,44 @@
 import Vue from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 import App from './App.vue'
 
-Vue.config.productionTip = false
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import VueApollo from 'vue-apollo'
+
+const httpLink = createHttpLink({
+  // GraphQLサーバのURIを指定
+  uri: 'http://localhost:4000/graphql',
+})
+
+// クエリキャッシュの取得
+const cache = new InMemoryCache()
+
+// Apollo client インスタンス生成
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache,
+})
+
+
+// Bootstrap
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+
+// Vue Apollo
+Vue.use(VueApollo);
+
+Vue.config.productionTip = false;
+
+// クライアント側からGraphQLのデータを読み込む
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+})
 
 new Vue({
+  apolloProvider,
   render: h => h(App),
 }).$mount('#app')
